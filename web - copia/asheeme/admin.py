cf390ba25d4c5e3ee94c7e1d3cb0b694e9cb65af
Leonardo Pizarro <49206331+LeoPizarroG@users.noreply.github.com>
2021-07-08 01:24:39 -0400

    Eliminacion de Copia.

--- web - copia/asheeme/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-# Register your models here.
-admin.site.register(Marca)
-admin.site.register(Producto)
--- web - copia/asheeme/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AsheemeConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'asheeme'
-    verbose_name = 'Admin Productos'
